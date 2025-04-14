5e61595aa52502d15db771c97e71e87e85335c91
adivs1102 <aadityavir11@gmail.com>
2025-04-14 13:01:24 +0530

    first commit

---
+++ app/(main)/dashboard/layout.js
@@ -0,0 +1,18 @@
+import DashboardPage from "./page";
+import Loading from "./loading";
+import { Suspense } from "react";
+
+export default function Layout() {
+  return (
+    <div className="px-5">
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold tracking-tight gradient-title">
+          Dashboard
+        </h1>
+      </div>
+      <Suspense fallback={<Loading />}>
+        <DashboardPage />
+      </Suspense>
+    </div>
+  );
+}
